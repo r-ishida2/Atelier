81828abf3c72083b1bcf2688b0eac3db162b428a
OugaTodaka <fko2347017@stu.o-hara.ac.jp>
2025-02-07 17:09:34 +0900

    appのモデル(仮)

--- tunatoriproject/tunatoriapp/models.py
+++ tunatoriproject/tunatoriapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# 投稿
+class Publish(models.Model):
+    user_id = models.ForeignKey()
+
+# カテゴリー
+class Category(models.Model):
+    category_name = models.CharField()
+
+# 返信
+class Reply(models.Model):
+    user_id = models.ForeignKey()
+
+# ブックマーク
+class Bookmark(models.Model):
+    user_id = models.ForeignKey()
