d77250a38fb7b9a15e41ca1cf415ec503c018d58
r-ishida2 <fko2447073@stu.o-hara.ac.jp>
2025-02-07 16:57:34 +0900

    accountアプリsettings

--- tunatoriproject/tunatoriproject/settings.py
+++ tunatoriproject/tunatoriproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tunatoriapp.apps.TunatoriappConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
