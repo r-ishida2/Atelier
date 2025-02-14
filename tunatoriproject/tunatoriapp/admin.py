5f3e36c4a385b0fceb55c15dcac93f3cb6fe82b0
r-ishida2 <fko2447073@stu.o-hara.ac.jp>
2025-02-14 16:32:44 +0900

    login.css

--- tunatoriproject/tunatoriapp/admin.py
+++ tunatoriproject/tunatoriapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Publish,Reply
 # Register your models here.
+
+class PublishAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title')
+  list_displaylinks = ('id', 'name')
+admin.site.register(Publish,PublishAdmin)
+
+admin.site.register(Reply)
